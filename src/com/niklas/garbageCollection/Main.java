df770e810b241a1878181e8bddd09e2d509923c3
Niklasodell <144327353+Niklasodell@users.noreply.github.com>
2023-10-10 12:01:12 +0200

    Third commit

--- src/com/niklas/garbageCollection/Main.java
+++ src/com/niklas/garbageCollection/Main.java
@@ -2,11 +2,29 @@ package com.niklas.garbageCollection;
 
 public class Main {
 
+    /* - Deprecated
+    public void finalize() {
+        System.out.println("Test Garbage Collection");
+    }
+    */
+
     public static void main(String[] args) {
 
         Counter counterA = new Counter();
         Counter counterB = new Counter();
+        Counter counterC = new Counter();
+        Counter counterD = new Counter();
 
+
+        // Garbage Collection - Mark for unreferenced Object (heap)
+        new Counter(); // Anonymous Object
+        counterC = null; // Nulling Reference
+        counterC = counterD; // Assigning Reference (to another)
+
+        System.out.println(counterC);
+        System.out.println(counterD);
+
+        System.gc();
 
 
         for (int i = 0; i < 5; i++) {
